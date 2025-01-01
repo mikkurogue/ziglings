646069f6b39fc1153854fe6e6d16dc2cb5ee4a19
mikku@arch <michael.lindemans@outlook.com>
2025-01-01 17:59:37 +0100

    ziglings p1 solutions up to 046 - optionals 2

---
+++ exercises/040_pointers2.zig
@@ -0,0 +1,34 @@
+//
+// It's important to note that variable pointers and constant pointers
+// are different types.
+//
+// Given:
+//
+//     var foo: u8 = 5;
+//     const bar: u8 = 5;
+//
+// Then:
+//
+//     &foo is of type "*u8"
+//     &bar is of type "*const u8"
+//
+// You can always make a const pointer to a mutable value (var), but
+// you cannot make a var pointer to an immutable value (const).
+// This sounds like a logic puzzle, but it just means that once data
+// is declared immutable, you can't coerce it to a mutable type.
+// Think of mutable data as being volatile or even dangerous. Zig
+// always lets you be "more safe" and never "less safe."
+//
+const std = @import("std");
+
+pub fn main() void {
+    const a: u8 = 12;
+    const b: *const u8 = &a;
+    std.debug.print("a: {}, b: {}\n", .{ a, b.* });
+}
+
+// this has multiple solutions
+// either changing `const a` to `var a`
+// or deferencing the pointer by doing a.*
+// or changing `const b` to be of type constant pointer ref of u8 (*const u8)
+// interesting i wonder what the best way to do this is
